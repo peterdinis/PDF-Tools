821bbdb6e9f7f3639704ff68919c02eac327ac53
peterdinis <petter1@pobox.sk>
2025-10-06 22:34:48 +0200

    feat: Pdf to powerpoint WIP

---
+++ app/pdf-to-powerpoint/page.tsx
@@ -0,0 +1,8 @@
+import PDFToPowerPointWrapper from "@/components/pdfs/pdf-to-powerpoint/PDFToPowerpointWrapper";
+import { NextPage } from "next";
+
+const PDFToPowerPointPage: NextPage = () => {
+    return <PDFToPowerPointWrapper />
+}
+
+export default PDFToPowerPointPage
