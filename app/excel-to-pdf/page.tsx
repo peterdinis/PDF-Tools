2280f8251b3dbd23167489e48403e4fdf457b274
peterdinis <petter1@pobox.sk>
2025-10-07 12:41:58 +0200

    feat: Excel to pdf

---
+++ app/excel-to-pdf/page.tsx
@@ -0,0 +1,8 @@
+import ExcelToPdfWrapper from "@/components/pdfs/excel-to-pdf/ExcelToPdfWrapper";
+import { NextPage } from "next";
+
+const ExcelToPdfPage: NextPage = () => {
+    return <ExcelToPdfWrapper />
+}
+
+export default ExcelToPdfPage
