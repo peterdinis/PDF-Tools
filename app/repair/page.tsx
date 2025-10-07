f25c351c0abf802c152bd2a1946c6f22d3b4923a
peterdinis <petter1@pobox.sk>
2025-10-07 15:33:40 +0200

    FOO

---
+++ app/repair/page.tsx
@@ -0,0 +1,8 @@
+import RepairWrapper from "@/components/pdfs/repair/RepairWrapper";
+import { NextPage } from "next";
+
+const RepairPdfPage: NextPage = () => {
+    return <RepairWrapper />
+}
+
+export default RepairPdfPage
---
+++ app/scan/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const ScanPdfPage: NextPage = () => {
+    return 
+}
+
+export default ScanPdfPage
