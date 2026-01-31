bb6de2c118b5bb44f1d0cc7d4ba0ef7fb90ff011
peterdinis <petter1@pobox.sk>
2026-01-31 20:45:28 +0100

    infra: Updating next-config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  typedRoutes: true,
-  experimental: {
-    browserDebugInfoInTerminal: true,
-    viewTransition: true,
-    typedEnv: true,
-    optimizeCss: true,
-  },
+	reactStrictMode: true,
+	devIndicators: {
+		position: "bottom-right",
+	},
+
+	typedRoutes: true,
+	experimental: {
+		optimizeCss: true,
+		browserDebugInfoInTerminal: true,
+	},
+	poweredByHeader: false,
+	generateEtags: true,
+	compress: true,
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-DNS-Prefetch-Control',
+						value: 'on',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN',
+					},
+				],
+			},
+		];
+	}
 };
 
 export default nextConfig;
